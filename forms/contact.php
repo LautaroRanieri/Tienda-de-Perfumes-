d29acab8ecac48ed44bdddc714a01c818ea5930d
unknown <photty705@gmail.com>
2023-09-04 18:40:19 -0300

    zipit

---
+++ forms/contact.php
@@ -0,0 +1,13 @@
+<?php
+$to = "zipitinsumos@gmail.com"; 
+      $from = $_POST['email']; 
+      $first_name = $_POST['name'];
+      $subject = $_POST['subject'];
+      $message =  $_POST['message'];
+      $headers = "From:" . $from;
+      $headers2 = "From:" . $to;
+      mail($to,$subject,$message,$headers);
+//      mail($from,$subject,$message,$headers2); // sends a copy of the message to the sender
+//      echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+echo "¡El formulario se ha enviado con éxito!";
+?>
